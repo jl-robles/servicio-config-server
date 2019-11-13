4399c407f47d8c560b48c5607133358175bb1528
jl-robles <luis.robles731@gmail.com>
2019-11-12 23:11:43 -0600

    first commit

---
+++ src/test/java/com/jlrm/springboot/app/config/ServicioConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jlrm.springboot.app.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
